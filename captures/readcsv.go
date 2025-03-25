ec0b0de344b01d9e990a991b8b9d48de5357ff0a
aaronsp777 <aaron777@gmail.com>
2025-03-24 17:53:37 -0700

    Decode traces from scope

--- captures/readcsv.go
+++ captures/readcsv.go
@@ -48,6 +48,7 @@ func readFile(filename string) error {
 }
 
 func parseRec(r []string, columns int, increment float64) error {
+	_ = columns
 	if len(r) < 4 {
 		return fmt.Errorf("too few cells in row %v", r)
 	}
@@ -68,7 +69,7 @@ func parseRec(r []string, columns int, increment float64) error {
 	if err != nil {
 		return err
 	}
-	decode(t, ch1, ch2, ch3)
+	decode(seq, t, ch1, ch2, ch3)
 	return nil
 }
 
@@ -80,29 +81,32 @@ var a, c, d bool // atn, clk, data
 var oa, oc, od bool
 var ot float64
 
-func decode(t float64, ch1, ch2, ch3 float64) {
+func decode(s int64, t float64, ch1, ch2, ch3 float64) {
 	a, c, d = ttl(ch1), ttl(ch2), ttl(ch3)
 	if oa != a {
-		fmt.Println(timestamp(t), "ATN", a)
+		fmt.Println(timestamp(s, t), "ATN", a)
 	}
 	if c && !oc { // rising CLK
 		if d {
-			fmt.Println(timestamp(t), "DATA", 1)
+			fmt.Println(timestamp(s, t), "DATA", 1)
 		} else {
-			fmt.Println(timestamp(t), "DATA", 0)
+			fmt.Println(timestamp(s, t), "DATA", 0)
 		}
 	}
 	oa, oc, od = a, c, d
 }
 
-func timestamp(t float64) string {
+func timestamp(seq int64, t float64) string {
 	d := t - ot
 	ot = t
-	return fmt.Sprintf("%6.2fms +%.2fms", t*1000, d*1000)
+	if d > 0.001 {
+		fmt.Println()
+	}
+	return fmt.Sprintf("%6.2fms +%.2fms %5d", t*1000, d*1000, seq)
 }
 
 func main() {
-	err := readFile("status.csv")
+	err := readFile("status_big.csv")
 	if err != nil {
 		log.Fatal(err)
 	}
